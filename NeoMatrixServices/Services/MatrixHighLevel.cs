31fb37eab7dfb58675411aca0e662a337e8ecd5b
Maxim <cloudyfg@gmail.com>
2022-10-19 18:34:52 +0300

    Refactored. Changed determinant. Added rank, reverse matrix.

---
+++ NeoMatrixServices/Services/MatrixHighLevel.cs
@@ -0,0 +1,18 @@
+﻿
+
+using DotNetMatrix;
+
+namespace NeoMatrixServices.Services;
+
+public class MatrixHighLevel
+{
+
+    public static double GetDeterminant(double[][] matrix) 
+        => new GeneralMatrix(matrix).Determinant();
+
+    public static double GetRank(double[][] matrix) 
+        => new GeneralMatrix(matrix).Rank();
+
+    public static GeneralMatrix GetReverseMatrix(double[][] matrix) 
+        => new GeneralMatrix(matrix).Inverse();
+}
--- TestMatrixServices/UnitTest1.cs
+++ TestMatrixServices/UnitTest1.cs
@@ -1,49 +1,46 @@
+using DotNetMatrix;
 using NeoMatrixServices.Services;
 
 namespace TestMatrixServices;
 
 public class Tests
 {
-    private Determinant _determinant;
     [SetUp]
     public void Setup()
     {
-        _determinant = new();
     }
 
     [Test]
     public void TestDeterminant()
     {
-        var expected = -5;
-        var matrix = new double[,]
+        var expected = 3;
+        double[][] matrix =
         {
-            { 3, 3, 4 },
-            { 1, 2, 4 },
-            { 3, 4, 5 }
+            new double[] {1,2,3},
+            new double[] {4,5,6},
+            new double[] {7,8,10}
         };
-        var actual = Determinant.Order3(matrix);
+        var actual = MatrixHighLevel.GetRank(matrix);
         Assert.AreEqual(expected, actual);
     }
 
     [Test]
-    public void TestSize()
+    public void Matrix()
     {
-        var expected = 3;
-        var matrix = new double[,]
+        double[][] matrix =
         {
-            { 3, 3, 4 },
-            { 1, 2, 4 },
-            { 3, 4, 5 }
+            new double[] {1,2,3},
+            new double[] {4,5,6},
+            new double[] {7,8,10}
         };
-        var actual1 = Determinant.EnoughSize(matrix, 1);
-        var actual2 = Determinant.EnoughSize(matrix, 2);
-        var actual3 = Determinant.EnoughSize(matrix, 3);
-        var actual4 = Determinant.EnoughSize(matrix, 4);
-        var actual5 = Determinant.EnoughSize(matrix, 5);
-        Assert.AreEqual(true, actual1);
-        Assert.AreEqual(true, actual2);
-        Assert.AreEqual(true, actual3);
-        Assert.AreEqual(false, actual4);
-        Assert.AreEqual(false, actual5);
+        double[][] revMatrix =
+        {
+            new double[] {1,2,3},
+            new double[] {0,1,2},
+            new double[] {0,0,1}
+        };
+        var expected = new GeneralMatrix(revMatrix);
+        var actual = MatrixHighLevel.GetReverseMatrix(matrix);
+        Assert.AreEqual(expected, actual);
     }
 }
