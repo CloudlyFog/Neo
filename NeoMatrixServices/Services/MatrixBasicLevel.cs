226426f29a87c27c820e8da77c627b8e5a8a8eb4
Maxim <cloudyfg@gmail.com>
2022-10-20 17:05:23 +0300

    Added basic operations for matrix. Add, subtract and multiply.

---
+++ NeoMatrixServices/Services/MatrixBasicLevel.cs
@@ -0,0 +1,15 @@
+﻿using DotNetMatrix;
+
+namespace NeoMatrixServices.Services;
+
+public class MatrixBasicLevel
+{
+    public static GeneralMatrix Add(double[][] matrix1, double[][] matrix2) 
+        => new GeneralMatrix(matrix1).Add(new GeneralMatrix(matrix2));
+
+    public static GeneralMatrix Subtract(double[][] matrix1, double[][] matrix2)
+        => new GeneralMatrix(matrix1).Subtract(new GeneralMatrix(matrix2));
+    
+    public static GeneralMatrix Multiply(double[][] matrix1, double[][] matrix2)
+        => new GeneralMatrix(matrix1).Multiply(new GeneralMatrix(matrix2));
+}
