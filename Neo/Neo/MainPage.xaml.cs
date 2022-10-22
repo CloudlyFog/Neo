6284f1729d005e23d9b52ee2656e9451f4605ace
Maxim <cloudyfg@gmail.com>
2022-10-22 17:38:43 +0300

    added start layout.

--- Neo/Neo/MainPage.xaml.cs
+++ Neo/Neo/MainPage.xaml.cs
@@ -4,6 +4,7 @@ using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using NeoServices.Services;
 using Xamarin.Forms;
 
 namespace Neo
@@ -13,6 +14,28 @@ namespace Neo
         public MainPage()
         {
             InitializeComponent();
+            SetMatrixFrames();
+        }
+
+
+        private void Exp_OnClick(object sender, EventArgs e)
+        {
+            var matrix = new double[][]
+            {
+                new double[] { }
+            };
+            var n = new int();
+            var output = MatrixHighLevel.Exponentiation(matrix, n);
+        }
+
+        private void SetMatrixFrames()
+        {
+            MatrixFrame.BackgroundColor = Color.White;
+            MatrixFrame.CornerRadius = 21;
+            MatrixFrame.HeightRequest = 250;
+            MatrixFrame.WidthRequest = 100;
+            MatrixFrame.Margin = 
+                new Thickness(33, 105, 38, 0);
         }
     }
 }
