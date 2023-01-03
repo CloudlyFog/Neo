6e60eee6628c3a3a250fec295f36e7704309fd7d
mxdnxght <cloudyfg@gmail.com>
2023-01-03 17:00:43 +0300

    added parser of equations.

--- Neo/Neo/Services/EquationParser.cs
+++ Neo/Neo/Services/EquationParser.cs
@@ -1,8 +1,5 @@
 ﻿using System.Text;
-using System.Text.RegularExpressions;
 using MathNet.Numerics.LinearAlgebra;
-using AngouriMath;
-using AngouriMath.Extensions;
 
 namespace Neo.Services;
 
@@ -27,14 +24,24 @@ public static partial class ListExtension
         var sb = new StringBuilder();
         for (var i = 0; i < input.Length; i++)
         {
+            // if input[i] neither is digit nor is split symbol ";"
+            // cycle is iterating
+            if (!char.IsDigit(input[i]) && input[i] != MatrixParser.SplitSymbol)
+                continue;
+
+            // else adds to string builder input[i] 
             if (char.IsDigit(input[i]) || input[i] == MatrixParser.SplitSymbol)
                 sb.Append(input[i]);
-            else
+
+            // stop cycle if "i" more than length of input string
+            if (i >= input.Length - 1)
+                break;
+
+            // if next element of input neither is digit nor is split symbol ";"
+            // adds whitespace
+            if (!char.IsDigit(input[i + 1]) && input[i + 1] != MatrixParser.SplitSymbol)
                 sb.Append(' ');
         }
-
-        Entity expr = "2 / 5 + 6";
-
 
         return sb.ToString();
     }
