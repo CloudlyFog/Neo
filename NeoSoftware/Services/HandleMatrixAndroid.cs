c07d36ef2822b2cc0445757d556e28495371c3dc
mxdnxght <cloudyfg@gmail.com>
2023-02-12 15:14:08 +0300

    moved logic of validation and added comments in HandleMatrixAndroid.cs

--- NeoSoftware/Services/HandleMatrixAndroid.cs
+++ NeoSoftware/Services/HandleMatrixAndroid.cs
@@ -1,10 +1,12 @@
 ﻿using System.Collections.Generic;
 using System.Drawing;
+using System.Linq;
 using Android.Content;
 using Android.Graphics.Drawables;
 using Android.Views;
 using Android.Widget;
 using MathNet.Numerics.LinearAlgebra;
+using Neo.Utilities;
 
 namespace NeoSoftware.Services
 {
@@ -13,6 +15,7 @@ namespace NeoSoftware.Services
         /// <summary>
         /// converting data from IGridListView to double[,]
         /// </summary>
+        /// <param name="gridLayout">current grid of ui</param>
         /// <returns></returns>
         public static Matrix<double> GetMatrix(GridLayout gridLayout)
         {
@@ -30,12 +33,21 @@ namespace NeoSoftware.Services
             return Matrix<double>.Build.DenseOfArray(matrix);
         }
 
+        /// <summary>
+        /// gets <see cref="GridLayout"/> with values of children from passed matrix"/>
+        /// </summary>
+        /// <param name="matrix">matrix with current values</param>
+        /// <param name="oldGridLayout">current <see cref="GridLayout"/> to which children will adds values</param>
+        /// <param name="context">context of app for adding children to <see cref="oldGridLayout"/></param>
+        /// <returns></returns>
         public static GridLayout GetGridLayout(Matrix<double> matrix, GridLayout oldGridLayout, Context context)
         {
-            if (oldGridLayout is null || matrix is null || context is null)
+            if (!ValidObtainingGridLayout(matrix, oldGridLayout, context))
                 return null;
+            if (!ValidMatrixValues(oldGridLayout))
+                return null;
+
             oldGridLayout.RemoveAllViews();
-            var test = new List<string>();
             var index = 0;
             for (var i = 0; i < oldGridLayout.RowCount; i++)
             {
@@ -56,6 +68,44 @@ namespace NeoSoftware.Services
             return oldGridLayout;
         }
 
+        /// <summary>
+        /// validates passed values of args 
+        /// </summary>
+        /// <param name="matrix">matrix with current values</param>
+        /// <param name="oldGridLayout">current <see cref="GridLayout"/> to which children will adds values</param>
+        /// <param name="context">context of app for adding children to <see cref="oldGridLayout"/></param>
+        /// <returns></returns>
+        private static bool ValidObtainingGridLayout(Matrix<double> matrix, GridLayout oldGridLayout, Context context)
+        {
+            if (matrix is null)
+            {
+                Error.Message = $"{nameof(matrix)} is null";
+                return false;
+            }
+
+            if (oldGridLayout is null)
+            {
+                Error.Message = $"{nameof(oldGridLayout)} is null";
+                return false;
+            }
+
+            if (context is null)
+            {
+                Error.Message = $"{nameof(context)} is null";
+                return false;
+            }
+
+            return true;
+        }
+
+        /// <summary>
+        /// validates passed indices
+        /// </summary>
+        /// <param name="i">referenced value of i (index of rows)</param>
+        /// <param name="j">referenced value of j (index of columns)</param>
+        /// <param name="rows">count of rows</param>
+        /// <param name="columns">count of columns</param>
+        /// <returns></returns>
         private static bool ValidateIterators(ref int i, ref int j, int rows, int columns)
         {
             // if i equals count of columns
@@ -73,6 +123,35 @@ namespace NeoSoftware.Services
             return j == rows;
         }
 
+        /// <summary>
+        /// validates values of grid's children
+        /// </summary
+        /// <param name="gridLayout">current grid of ui</param>
+        /// <returns></returns>
+        private static bool ValidMatrixValues(GridLayout? gridLayout)
+        {
+            for (var i = 0; i < gridLayout.ChildCount; i++)
+            {
+                var child = (EditText)gridLayout.GetChildAt(i);
+                if (child.Text.Any(char.IsDigit))
+                {
+                    Error.Message = "values of matrix contains non digit symbols. Please check entered values.";
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
+        /// <summary>
+        /// sets values from <see cref="GridLayout"/>'s children to matrix
+        /// </summary>
+        /// <param name="matrix">matrix where adds values</param>
+        /// <param name="gridLayout">grid from which takes values to add to matrix</param>
+        /// <param name="startPoint">index of <see cref="GridLayout"/>'s children</param>
+        /// <param name="i">index of matrix's rows</param>
+        /// <param name="j">index of matrix's columns</param>
+        /// <returns></returns>
         private static double[,] SetMatrix(double[,] matrix, GridLayout gridLayout, int startPoint, int i, int j)
         {
             // get frame with index startPoint from array of MatrixGrid
